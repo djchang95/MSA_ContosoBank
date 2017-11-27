39c50aa1c3c74fbe89ac3c8b8b95e168374f7eb8
djchang95 <jimmy950928@gmail.com>
2017-11-27 18:31:10 +1300

    updated

--- MSA_ContosoBank/MSA_ContosoBank/Controllers/MessagesController.cs
+++ MSA_ContosoBank/MSA_ContosoBank/Controllers/MessagesController.cs
@@ -5,12 +5,18 @@ using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.IO;
+using MSA_ContosoBank.Services;
 
 namespace MSA_ContosoBank
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+
+        private readonly MicrosoftCognitiveSpeechService speechService = new MicrosoftCognitiveSpeechService();
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -19,18 +25,66 @@ namespace MSA_ContosoBank
         {
             if (activity.Type == ActivityTypes.Message)
             {
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                string mymessage;
+
+                try
+                {
+                    var audioAttachment = activity.Attachments?.FirstOrDefault(a => a.ContentType.Equals("audio/wav") || a.ContentType.Equals("application/octet-stream"));
+
+                    if (audioAttachment != null)
+                    {
+                        var stream = await GetImageStream(connector, audioAttachment);
+                        var text = await this.speechService.GetTextFromAudioAsync(stream);
+                        mymessage = ProcessText(text);
+                    }
+                    else
+                    {
+                        await Conversation.SendAsync(activity, () => new Dialogs.LuisDialog());
+                    }
+                }
+                catch
+                {
+                    mymessage = "Something went wrong with Audio processing";
+                }
+
+
                 await Conversation.SendAsync(activity, () => new Dialogs.LuisDialog());
             }
             else
             {
                 HandleSystemMessage(activity);
             }
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
+        private static string ProcessText(string text)
+        {
+            string message = "You said : " + text + ".";
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                var wordCount = text.Split(' ').Count(x => !string.IsNullOrEmpty(x));
+                message += "\n\nWord Count: " + wordCount;
+
+                var characterCount = text.Count(c => c != ' ');
+                message += "\n\nCharacter Count: " + characterCount;
+
+                var spaceCount = text.Count(c => c == ' ');
+                message += "\n\nSpace Count: " + spaceCount;
+
+                var vowelCount = text.ToUpper().Count("AEIOU".Contains);
+                message += "\n\nVowel Count: " + vowelCount;
+            }
+
+            return message;
+        }
+
         private Activity HandleSystemMessage(Activity message)
         {
+
             if (message.Type == ActivityTypes.DeleteUserData)
             {
                 // Implement user deletion here
@@ -41,6 +95,15 @@ namespace MSA_ContosoBank
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
+                if (message.MembersAdded.Any(m => m.Id == message.Recipient.Id))
+                {
+                    var connector = new ConnectorClient(new Uri(message.ServiceUrl));
+
+                    var response = message.CreateReply();
+                    response.Text = "Hi!, Welcome to Contoso Bank Bot service. Please say Hi back to me to begin your service";
+
+                    connector.Conversations.ReplyToActivity(response);
+                }
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
@@ -53,6 +116,35 @@ namespace MSA_ContosoBank
             }
             else if (message.Type == ActivityTypes.Ping)
             {
+            }
+
+            return null;
+        }
+
+        private static async Task<Stream> GetImageStream(ConnectorClient connector, Attachment imageAttachment)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // The Skype attachment URLs are secured by JwtToken,
+                // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+                // https://github.com/Microsoft/BotBuilder/issues/662
+                var uri = new Uri(imageAttachment.ContentUrl);
+                if (uri.Host.EndsWith("skype.com") && uri.Scheme == "https")
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetTokenAsync(connector));
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+                }
+
+                return await httpClient.GetStreamAsync(uri);
+            }
+        }
+
+        private static async Task<string> GetTokenAsync(ConnectorClient connector)
+        {
+            var credentials = connector.Credentials as MicrosoftAppCredentials;
+            if (credentials != null)
+            {
+                return await credentials.GetTokenAsync();
             }
 
             return null;
